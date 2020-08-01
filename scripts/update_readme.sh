3e6031b4c8986aa2cf6c974bcb68560ec63b734f
Meir Gabay <meir@prodops.io>
2020-08-01 04:16:45 +0300

    rm -it

--- scripts/update_readme.sh
+++ scripts/update_readme.sh
@@ -4,7 +4,7 @@ DST_FILE_PATH=README.md
 DOCKER_FOLDER=/app
 DOCKER_SRC_PATH="${DOCKER_FOLDER}"/"${SRC_FILE_PATH}"
 DOCKER_DST_PATH="${DOCKER_FOLDER}"/"${DST_FILE_PATH}"
-DOCKER_TAG=unfor19/replacer:latest
+DOCKER_TAG=unfor19/replacer
 
 REPLACER_START_VALUE="<!-- replacer_start_help -->"
 REPLACER_END_VALUE="<!-- replacer_end_help -->"
@@ -14,7 +14,7 @@ docker build -f Dockerfile "${DOCKER_TAG}" .
 docker run --name replacer_help "${DOCKER_TAG}" --help
 help_message=$(docker logs replacer_help)
 
-echo -e "\`\`\`bash\n\\$ docker run --rm -it -v \\$\{PWD\}/:/app ${DOCKER_TAG} --help" > "${SRC_FILE_PATH}"
+echo -e "\`\`\`bash\n\\$ docker run --rm -v \\$\{PWD\}/:/app ${DOCKER_TAG} --help" > "${SRC_FILE_PATH}"
 echo -e "${help_message}" >> "${SRC_FILE_PATH}"
 echo -e "\n\`\`\`" >> "${SRC_FILE_PATH}"
 
